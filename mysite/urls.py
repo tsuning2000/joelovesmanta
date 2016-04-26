7343acb85c5c23f62ec60b0be3c1a62f5b4bf417
祖寧波 蔡 <Joe@zuningbodeMacBook-Pro.local>
2016-04-27 02:51:48 +0800

    Final

--- mysite/urls.py
+++ mysite/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     url(r'^$', personal_views.index, name='index'),
     url(r'^blog/', blog_views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', blog_views.post_detail, name='post_detail'),
+    url(r'^post/new/$', blog_views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', blog_views.post_edit, name='post_edit'),
 ]
